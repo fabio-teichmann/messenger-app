62b4aefda16c33e2ead605b0cb4153878d6dbf58
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-06-05 14:57:53 +0300

    bugfixed

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"messenger-app/models"
 	"sync"
+	"time"
 )
 
 type Event struct {
@@ -32,7 +33,7 @@ type EventSubject struct {
 }
 
 func (subscriber *EventSubscriber) NotifyCallback(event Event) {
-	fmt.Printf("Sender: %v, Receiver: %v, Time: %v, Message %s\n", event.SubjectID, event.TargetID, event.Data.Time, event.Data.Body)
+	fmt.Printf("Sender: %v, Target: %v, Receiver: %v, Time: %v, Message %s\n", event.SubjectID, event.TargetID, subscriber.ID, event.Data.Time, event.Data.Body)
 }
 
 func (subject *EventSubject) AddSubscriber(sub EventSubscriber) {
@@ -45,19 +46,68 @@ func (subject *EventSubject) RemoveSubscriber(sub EventSubscriber) {
 
 func (es *EventSubject) NotifySubscriber(event Event) {
 	es.Observers.Range(func(key interface{}, value interface{}) bool {
-
-		if key == nil || value == nil {
-			fmt.Printf("could not find matching Subscriber to event id: %v", event.SubjectID)
+		fmt.Println(key)
+		if key == nil {
+			fmt.Printf("could not find matching Subscriber with id %v to event id: %v", event.TargetID, event.SubjectID)
 			return false
 		}
+		es := key.(EventSubscriber)
 
-		if key.(EventSubscriber).ID == event.TargetID {
+		if es.ID == event.TargetID {
 			// found matching subscriber
-			key.(EventSubscriber).NotifyCallback(event)
+			es.NotifyCallback(event)
+			return false
 		}
+		return true
 	})
+	// fmt.Printf("could not find matching Subscriber with id %v to event id: %v\n", event.TargetID, event.SubjectID)
 }
 
-func main() {
+// func SendMessage(sender EventSubscriber, target EventSubject, message models.Message) {
+// 	event := Event{
+// 		SubjectID: sender.ID,
+// 		TargetID:  target.ID,
+// 		Data:      message,
+// 	}
 
+// }
+
+func main() {
+	var user1 = EventSubscriber{ID: 1}
+	var user2 = EventSubscriber{ID: 2}
+	var user3 = EventSubscriber{ID: 3}
+
+	var chat12 = EventSubject{
+		ID:        12,
+		Observers: sync.Map{},
+	}
+	var chat13 = EventSubject{
+		ID:        13,
+		Observers: sync.Map{},
+	}
+
+	// subscribing users to chats
+	chat12.AddSubscriber(user2)
+	chat12.AddSubscriber(user1)
+
+	chat13.AddSubscriber(user3)
+	chat13.AddSubscriber(user1)
+
+	for _, chat := range []*EventSubject{&chat13, &chat12, &chat13} {
+		message := models.Message{
+			Body: fmt.Sprintf("%s_%v", "test message", chat.ID),
+			Time: time.Now(),
+		}
+
+		event := Event{
+			SubjectID: user1.ID,
+			TargetID:  chat.ID % 10,
+			Data:      message,
+		}
+		// fmt.Println(event)
+
+		chat.NotifySubscriber(event)
+		// user1.NotifyCallback(event)
+
+	}
 }
