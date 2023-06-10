7a91cd59aede7d8358cbe3b0637863f5c2c901be
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-06-10 11:55:48 +0300

    setup MongoDB connection

---
+++ storage/mongodb.go
@@ -0,0 +1,53 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoConfig struct {
+	DBName   string
+	User     string
+	Password string
+	Port     string
+}
+
+func NewMongoConnection(config *MongoConfig) (*mongo.Client, func(), error) {
+	uri := fmt.Sprintf(
+		"mongodb+srv://%s:%s@%s/?retryWrites=true&w=majority",
+		config.User, config.Password, config.DBName,
+	)
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	// instanciate MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, opts)
+
+	if err != nil {
+		fmt.Println("unable to connect to db")
+		return nil, nil, err
+	}
+
+	// defer closing
+	closer := func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}
+
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	return client, closer, nil
+}
