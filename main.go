7c7b90d344531b82f841b71dc0679d74d54abd33
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-06-17 23:53:55 +0300

    sample chat with 1 msg

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	defer close()
 
 	// make sure connection is established
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	if err := client.Ping(ctx, readpref.Primary()); err != nil {
 		fmt.Println("connection to mongodb not established")
@@ -43,8 +43,45 @@ func main() {
 	ctx = context.WithValue(ctx, models.TestCollectionKey, os.Getenv("TEST_COLLECTION_EVENTS"))
 	// fmt.Printf("context values - database: %v; collection: %v\n", ctx.Value(models.TestDBKey), ctx.Value(models.TestCollectionKey))
 
-	ac := models.NewAppControler(client)
+	ac := models.InitializeAppControler(client)
 
+	go ac.ReadEventMessages(ctx)
+
+	sub1 := models.NewEventSubscriber(models.User{ID: 1, Name: "user1"})
+	sub2 := models.NewEventSubscriber(models.User{ID: 2, Name: "user2"})
+
+	ac.MsgSent.AddSubscriber(sub1)
+	ac.MsgSent.AddSubscriber(sub2)
+
+	ac.MsgRcvd.AddSubscriber(sub1)
+	ac.MsgRcvd.AddSubscriber(sub2)
+
+	event, err := sub1.CreateEvent(models.MSG_SENT, models.NewMessage("Test message"), sub2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(event)
+
+	err = ac.AddEvent(ctx, event)
+	if err != nil {
+		fmt.Println(err)
+	}
+	time.Sleep(1 * time.Second)
+
+	ac.AcceptEvent(event)
+
+	for {
+		select {
+		// case result := <-results:
+		// 	fmt.Println(result)
+		case <-time.After(5 * time.Second):
+			fmt.Println("timed out")
+			ac.ControlChan <- models.DoExit
+			<-ac.ControlChan // wait for response from go routine (gives the go routine a chance to finish its work)
+			fmt.Println("program exit")
+			return
+		}
+	}
 	// event, err := ac.GetEventByMessageId(ctx, 0)
 	// if err != nil {
 	// 	fmt.Println(err)
@@ -78,50 +115,50 @@ func main() {
 	// }
 	// fmt.Println("# messages id 1:", count)
 
-	user1 := models.User{ID: 1, Name: "user1"}
-	user2 := models.User{ID: 2, Name: "user2"}
-	user3 := models.User{ID: 3, Name: "user3"}
+	// user1 := models.User{ID: 1, Name: "user1"}
+	// user2 := models.User{ID: 2, Name: "user2"}
+	// user3 := models.User{ID: 3, Name: "user3"}
 
-	count, err := ac.CountMessagesSentByUser(ctx, &user1)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("# messages sender id 1:", count)
+	// count, err := ac.CountMessagesSentByUser(ctx, &user1)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println("# messages sender id 1:", count)
 
-	count, err = ac.CountMessagesSentByUser(ctx, &user2)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("# messages sender id 2:", count)
+	// count, err = ac.CountMessagesSentByUser(ctx, &user2)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println("# messages sender id 2:", count)
 
-	count, err = ac.CountMessagesSentByUser(ctx, &user3)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("# messages sender id 3:", count)
+	// count, err = ac.CountMessagesSentByUser(ctx, &user3)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println("# messages sender id 3:", count)
 
-	// Create message
-	msg := models.NewMessage("Test Insert")
+	// // Create message
+	// msg := models.NewMessage("Test Insert")
 
-	event, err := models.NewEventSubscriber(user3).CreateEvent(models.MSG_SENT, &msg, models.NewEventSubscriber(user1))
-	if err != nil {
-		fmt.Println(err)
-	}
+	// event, err := models.NewEventSubscriber(user3).CreateEvent(models.MSG_SENT, &msg, models.NewEventSubscriber(user1))
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
-	err = ac.RemoveEventByMessageId(ctx, msg.ID)
-	if err != nil {
-		fmt.Println(err)
-	}
+	// err = ac.RemoveEventByMessageId(ctx, msg.ID)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
-	err = ac.AddEvent(ctx, event)
-	if err != nil {
-		fmt.Println(err)
-	}
-	msgUpdate := models.NewMessage("Updated Insert")
-	err = ac.UpdateEventMessageByMessageId(ctx, msg.ID, msgUpdate)
-	if err != nil {
-		fmt.Println(err)
-	}
+	// err = ac.AddEvent(ctx, event)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// msgUpdate := models.NewMessage("Updated Insert")
+	// err = ac.UpdateEventMessageByMessageId(ctx, msg.ID, msgUpdate)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
 	// err = ac.RemoveEventByMessageId(ctx, msg.ID)
 	// if err != nil {
